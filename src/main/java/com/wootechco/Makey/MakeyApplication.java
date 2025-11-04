06898e3964dc3020533965928a42eb5dab3bc54a
jngbn <jngbnss@naver.com>
2025-11-04 14:17:31 +0900

    Initial commit

---
+++ src/main/java/com/wootechco/Makey/MakeyApplication.java
@@ -0,0 +1,13 @@
+package com.wootechco.Makey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wootechco/Makey/MakeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wootechco.Makey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
