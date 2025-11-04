e916c744c4e38e386fab956792fd8f423d69a540
jngbn <jngbnss@naver.com>
2025-11-04 15:31:59 +0900

    feat : PostController

---
+++ src/main/java/com/wootechco/Makey/board/controller/PostController.java
@@ -0,0 +1,48 @@
+package com.wootechco.Makey.board.controller;
+
+import com.wootechco.Makey.board.dto.PostRequestDto;
+import com.wootechco.Makey.board.dto.PostResponseDto;
+import com.wootechco.Makey.board.service.PostService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/posts")
+public class PostController {
+    private final PostService postService;
+
+    @PostMapping
+    public Long create(@RequestBody PostRequestDto dto) {
+        return postService.create(dto);
+    }
+
+    @GetMapping
+    public List<PostResponseDto> findAll() {
+        return postService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public PostResponseDto findById(@PathVariable Long id) {
+        return postService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable Long id, @RequestBody PostRequestDto dto) {
+        postService.update(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        postService.delete(id);
+    }
+
+}
