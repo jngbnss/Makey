77d22044840342c0745a3e9a8e8a6a30c98fa33a
jngbn <jngbnss@naver.com>
2025-11-05 03:40:41 +0900

    refactor : WebConfig 메서드 추가

--- src/main/java/com/wootechco/Makey/board/config/WebConfig.java
+++ src/main/java/com/wootechco/Makey/board/config/WebConfig.java
@@ -7,6 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+        registry.addMapping("/**").allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*"); // 메서드를 붙여야 되는거구나;
     }
 }
