14925bb983e32eb3edb167034fe8593e90ea00dc
jngbn <jngbnss@naver.com>
2025-11-05 05:02:24 +0900

    fix : WebConfig 연결 주소 오타

--- src/main/java/com/wootechco/Makey/config/WebConfig.java
+++ src/main/java/com/wootechco/Makey/config/WebConfig.java
@@ -7,8 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173")
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "http://localhost:5174") // 여러 포트 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*"); // 메서드를 붙여야 되는거구나;
+                .allowedHeaders("*");
     }
 }
+
