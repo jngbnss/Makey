6ebc46df996856f22fa143c0d3b32349cf00db24
jngbn <jngbnss@naver.com>
2025-11-06 13:07:03 +0900

    refactor: 메인화면 노인에서 실종된 치매 노인 위치 추적으로 변경

--- react-frontend/src/pages/Main.jsx
+++ react-frontend/src/pages/Main.jsx
@@ -7,20 +7,20 @@ export default function Main() {
   const items = [
     "프로필",
     "게시판",
-    "노인",
+    "실종된 치매 노인 위치 추적",
     "쇼핑",
     "생활/가정",
     "경제/재테크",
     "건강/의료",
     "행정/법률",
-    "쇼핑",
+    "채팅",
   ];
 
   const handleClick = (item) => {
     if (item === "게시판") {
       navigate("/posts"); // 게시판 클릭 시 게시판 목록 페이지로 이동
     }
-    if (item === "노인") {
+    if (item === "실종된 치매 노인 위치 추적") {
       navigate("/elderly");
     }
   };
