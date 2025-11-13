60c217e848488d6e538e2558270f3871e81044cd
jngbn <jngbnss@naver.com>
2025-11-13 21:10:04 +0900

    chore: aws 설정

--- src/test/java/com/wootechco/Makey/MakeyApplicationTests.java
+++ src/test/java/com/wootechco/Makey/MakeyApplicationTests.java
@@ -2,8 +2,10 @@ package com.wootechco.Makey;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "my.secret-key=test-secret")
 class MakeyApplicationTests {
 
 	@Test
